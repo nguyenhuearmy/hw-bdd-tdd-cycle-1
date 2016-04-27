267e7081c1a8d061363d4728d736666ecf21dbd6
nguyenhuearmy <huearmynguyen@gmail.com>
2016-04-27 14:03:04 +0000

    add search_director.feature

--- rottenpotatoes/db/migrate/20160427131918_add_more_movies.rb
+++ rottenpotatoes/db/migrate/20160427131918_add_more_movies.rb
@@ -1,22 +1,25 @@
 class AddMoreMovies < ActiveRecord::Migration
-  
-  More_Movies = [
-  {:title => 'Alandi', :rating => 'G', :release_date => '25-Nov-1992'},
-  {:title => 'Alaka', :rating => 'R', :release_date => '25-Nov-2000'},
-  {:title => 'Humos', :rating => 'PG-13', :release_date => '25-De-1992'},
-  {:title => 'Hero', :rating => 'G', :release_date => '25-Nov-1990'},
-  {:title => 'Al', :rating => 'PG', :release_date => '10-June-2000'},
-  {:title => 'Queen', :rating => 'G', :release_date => '30-Sep-1982'},
+  MORE_MOVIES = [
+    {:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
+    {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'},
+    {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
+    {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
+    {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'},
+    {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'},
+    {:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968'},
+    {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004'},
+    {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
+    {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000'},
   ]
-  
   def up
-    More_Movies.each do |movie|
+    Movie.delete_all
+    MORE_MOVIES.each do |movie|
       Movie.create!(movie)
     end
   end
-  
-  def dowm
-    More_Movies.each do |movie|
+
+  def down
+    MORE_MOVIES.each do |movie|
       Movie.find_by_title_and_rating(movie[:title], movie[:rating]).destroy
     end
   end
